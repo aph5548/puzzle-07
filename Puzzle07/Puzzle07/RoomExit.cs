9993c146640d3b671b2617b001a6d6772845990b
MSI\flame <mcc2337@g.rit.edu>
2017-04-19 16:51:30 -0400

    Made the object to change the room, called RoomExit, only works if the room is deemed complete.

---
+++ Puzzle07/Puzzle07/RoomExit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Michael Capra
+ * RoomExit Class
+ * If the player walks onto this and the room is complete, it changes the room
+ */
+
+namespace Puzzle07
+{
+    class RoomExit : GameObject
+    {
+        public RoomExit(int x, int y, int width, int height) : base(x, y, width, height)
+        {
+        }
+
+        public bool ChangeRoom(Player player, bool complete)
+        {
+            if(Position.Intersects(player.Position) && complete == true)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
